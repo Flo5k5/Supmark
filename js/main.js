142507fa00195740d2117aa5d27a51ecd1fd929c
Florian <florian.nourrisse@mail.sogeti.com>
2013-07-01 15:56:04 +0200

    Essai d'un premier design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,134 @@
+    var marker;
+
+        /*$(function() {
+            $( document ).ready(function() {
+                $("#mark-container").fadeOut().queue(function() {
+                    $("#mark-container").html('');
+                    loadGMaps();
+                    $(this).dequeue();
+                }).fadeIn();
+            });
+            
+        });*/
+
+        function initializeGMaps() {
+            navigator.geolocation.getCurrentPosition(mapsCallbackSuccess, mapsCallbackError, { enableHighAccuracy: true, timeout: 10000, maximumAge: Infinity });
+        }
+
+        function loadGMaps() {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBBvFQ4cJF4FegZ1vVgXrrJZ__djvMPdcE&sensor=false&libraries=weather&callback=initializeGMaps";
+            document.body.appendChild(script);
+        }
+
+        function mapsCallbackSuccess(position) {
+            console.log(position);
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var pos = new google.maps.LatLng(lat, lng);
+            console.log(lat);
+            console.log(lng);
+            var mapOptions = {
+                zoom: 7,
+                center: pos,
+                disableDefaultUI: true,
+                mapTypeControl: true,
+                mapTypeId: google.maps.MapTypeId.HYBRID
+            }
+            var map = new google.maps.Map(document.getElementById("gmaps"), mapOptions);
+
+            marker = new google.maps.Marker({
+                map:map,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                title: "You are here",
+                position: pos
+            });
+
+            setLayersMaps(map);
+            google.maps.event.addListener(marker, 'dragend', function(){
+                geocodePosition(marker.getPosition());
+            });
+        }
+
+        function mapsCallbackError(error) {
+            console.log('There was an error with GMaps : ');
+            console.log(error);
+
+            var pos = new google.maps.LatLng(25.165173,-23.90625);
+
+            var mapOptions = {
+                zoom: 2,
+                center: new google.maps.LatLng(25.165173,-23.90625),
+                disableDefaultUI: true,
+                mapTypeControl: true,
+                mapTypeId: google.maps.MapTypeId.HYBRID
+            }
+            var map = new google.maps.Map(document.getElementById("gmaps"), mapOptions);
+
+            marker = new google.maps.Marker({
+                map:map,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                title: "Positioning error",
+                position: pos
+            });
+
+            setLayersMaps(map);
+
+            google.maps.event.addListener(marker, 'dragend', function(){
+                geocodePosition(marker.getPosition());
+            });
+        }
+
+        function setLayersMaps(map){
+            var ctaLayer = new google.maps.KmlLayer(
+                'https://maps.google.fr/maps/ms?ie=UTF8&t=h&oe=UTF8&authuser=0&msa=0&output=kml&msid=212676093823261248407.000465792f3231179dcb6',
+                {   
+                    preserveViewport: true,
+                    suppressInfoWindows: false
+                }
+            );
+            //ctaLayer.setMap(map);
+            var weatherLayer = new google.maps.weather.WeatherLayer({
+              temperatureUnits: google.maps.weather.TemperatureUnit.CELSIUS
+            });
+            //weatherLayer.setMap(map);
+            var transitLayer = new google.maps.TransitLayer();
+            //transitLayer.setMap(map);
+            //map.setCenter(position);
+        }
+
+
+
+        function geocodePosition(pos) 
+        {
+           geocoder = new google.maps.Geocoder();
+           geocoder.geocode
+            ({
+                latLng: pos
+            }, 
+                function(results, status) 
+                {
+                    if (status == google.maps.GeocoderStatus.OK) 
+                    {
+                        console.log("Results : ", results);
+                        console.log("Adresse : ", results[0].formatted_address);
+                        console.log("Latitude : ", results[0].geometry.location.jb);
+                        console.log("Longitude : ", results[0].geometry.location.kb);
+                        //$("#mapSearchInput").val(results[0].formatted_address);
+                        //$("#mapErrorMsg").hide(100);
+                    } 
+                    else 
+                    {
+                        console.log("Cannot determine address at this location "+status);
+                        //$("#mapErrorMsg").html('Cannot determine address at this location.'+status).show(100);
+                    }
+                }
+            );
+        }
+
 $(function() {
 
 	window.Client = Backbone.Model.extend({
@@ -79,7 +210,8 @@ $(function() {
             "" : "root",
             "showAbout" : "showAbout",
             "hideAbout" : "hideAbout",
-            "login" : "login"
+            "login" : "login",
+            "gmaps" : "gmaps"
         },
 
         root : function() {
@@ -105,6 +237,16 @@ $(function() {
             $("#divClient").hide();
             $("#lnkAbout").hide();
             $("#login").fadeIn();
+
+        },
+
+        gmaps : function () {
+            console.log("Action : gmaps");
+            $("#divAbout").hide();
+            $("#divClient").hide();
+            $("#lnkAbout").hide();
+            $("#login").hide();
+            loadGMaps();
 
         }
 
